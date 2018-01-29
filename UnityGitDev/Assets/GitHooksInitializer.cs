380d3d9dfde71b68996e4bd9fec858e13bccb0ca
Amazon2011 <chenyongtao2011@gmail.com>
2018-01-30 00:48:39 +0800

    Add GitHooksInitializer.cs

--- UnityGitDev/Assets/GitHooksInitializer.cs
+++ UnityGitDev/Assets/GitHooksInitializer.cs
@@ -1,11 +1,34 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 [InitializeOnLoad]
 public class GitHooksInitializer
 {
-    GitHooksInitializer()
+    static GitHooksInitializer()
     {
-        Debug.Log("Up and running");
+        Debug.LogWarning("Up and running ");
+        Debug.LogWarning(Application.dataPath);
+
+        foreach (var fileName in Directory.GetFiles(Application.dataPath))
+        {
+            Debug.LogWarning("file name = " + fileName);
+        }
+
+        var commitMsgHookFilePath = "D:/study/github/Unity_Study/UnityGitDev/Assets/commit-msg";
+
+        var hooksDir = "D:/study/github/Unity_Study/.git/hooks";
+        var destCommitMsgHookFilePath = "D:/study/github/Unity_Study/.git/hooks/commit-msg";
+        var hookFileNames = Directory.GetFiles(hooksDir);
+        foreach (var fileName in hookFileNames)
+        {
+            if (fileName.EndsWith("\\commit-msg.sample"))
+            {
+                Debug.LogWarning("file name = " + fileName);
+            }
+        }
+
+        System.IO.File.Copy(commitMsgHookFilePath, destCommitMsgHookFilePath, true);
+
     }
 }
